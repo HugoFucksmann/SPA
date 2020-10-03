4f1d2a323feb9087b4abf93bd413930fa622f992
Hugo Fucksmann <hugoffuksmann@gmail.com>
2020-10-03 20:50:24 -0300

    upload git

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: '', redirectTo: '/inico', pathMatch: 'full' },
+  { path: '**', component: NopagefoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    InicioComponent,
+    NopagefoundComponent
   ],
   imports: [
     BrowserModule,
